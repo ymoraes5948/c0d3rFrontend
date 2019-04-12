73ee24c34bbd03e9bbfef8578c462e819de56cd9
yuri <ymoraes5948@gmail.com>
2019-04-11 23:06:46 -0300

    aula dia 10 37min

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,16 +1,37 @@
 import React, { Component } from 'react';
+import api from "../../services/api";
 
 import logo from '../../assets/logo.svg';
 import './styles.css';
 
 export default class Main extends Component {
-  render() {
+    state = {
+        newBox: '',
+    };
+    
+    handleSubmit = (e) => {
+        e.preventDefault();
+
+        const response = await api.post("boxes", {
+            title: this.state.newBox,
+        });
+
+        
+    }
+
+    handleInputChange = (e) => {
+        this.setState({ newBox: e.target.value });
+    }
+  
+    render() {
     return (
         <div id="main-container">
-            <form action="">
+            <form onSubmit={this.handleSubmit}>
                 <img src={logo} alt=""/>
                 <input 
-                placeholder="Criar um box" />
+                placeholder="Criar um box"
+                value={this.state.newBox}
+                onChange={this.handleInputChange} />
                 <button type="submit">Criar</button>
             </form>
         </div>
